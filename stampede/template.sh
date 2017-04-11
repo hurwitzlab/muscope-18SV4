e40956f9e12a9ee3f89711026aa2a8bf39485899
Joshua Lynch <jklynch@email.arizona.edu>
2017-04-11 14:40:06 -0700

    use working directory as output directory

--- stampede/template.sh
+++ stampede/template.sh
@@ -5,8 +5,7 @@ echo "Started $(date)"
 inputDir=${INPUT_DIR}
 echo "input directory: ${inputDir}"
 
-sh run.sh ${inputDir} ${outputDir}
+sh run.sh ${inputDir} `pwd`
 
 echo "Ended $(date)"
 exit 0
-
