1ce91438638ad32b4bc63bf9f43a21a983dd0f1d
Joshua Lynch <jklynch@email.arizona.edu>
2017-06-07 14:37:38 +0100

    make test option --uchime-ref-db-fp required

--- test/conftest.py
+++ test/conftest.py
@@ -2,7 +2,7 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--uchime-ref-db-fp", action="store",  help="path to PR2 database")
+    parser.addoption("--uchime-ref-db-fp", action="store", required=True, help="path to PR2 database")
 
 @pytest.fixture
 def uchime_ref_db_fp(request):
