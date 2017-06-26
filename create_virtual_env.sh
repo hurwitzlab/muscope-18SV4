ae414cd38fd36c95e6fcbcf56761042fcb7fcafe
Joshua Lynch <jklynch@email.arizona.edu>
2017-06-26 10:56:27 +0100

    add -y to create and install

--- create_virtual_env.sh
+++ create_virtual_env.sh
@@ -2,20 +2,20 @@
 
 VENV_NAME=18SV4
 
-if [ -d "~/miniconda3/envs/${VENV_NAME}" ]; then
-  conda remove -n ${VENV_NAME} --all
-fi
+#if [ -d "~/miniconda3/envs/${VENV_NAME}" ]; then
+  conda remove -y -n ${VENV_NAME} --all
+#fi
 
 # there is a problem with VirtualBox shared filesystems
 # that prevents a Python virtual environment from being created
 # in a host directory
 
-conda create -n ${VENV_NAME} python=2.7
+conda create -y -n ${VENV_NAME} python=2.7
 source activate ${VENV_NAME}
 conda update conda
 conda update pip
 
 # install numpy explicitly or biom-format will not install
-conda install numpy
+conda install -y numpy
 
 pip install -e .[test]
